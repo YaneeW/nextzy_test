ca1cea5eb491faf3c21c8bf96d7c6aa13174a426
yanee wiboonsak <yanee.w@ztrus.com>
2025-08-25 14:02:35 +0700

    edit loading file

--- client/src/app/loading.jsx
+++ client/src/app/loading.jsx
@@ -1,3 +1,8 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+      <div className="flex items-center justify-center w-full h-full mt-100">
+        <div className="lg:border-12 lg:h-30 lg:w-30 border-6 h-15 w-15 border-dotted border-red-700 animate-spin  relative rounded-full"> </div>
+        <div className="absolute text-xs lg:text-xl">Loading</div>
+      </div>
+  )
 }
